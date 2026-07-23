d8d1edbc272e7ea8f9866b454082f2b46eb157c1
bendar21 <bendarlington21@gmail.com>
2026-07-23 16:03:37 +1200

    feat: added auth functionality

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,11 +1,13 @@
 import { tabs } from "@/constants/data";
 import { colors, components } from "@/constants/theme";
+import { useAuth } from "@clerk/expo";
 import clsx from "clsx";
-import { Tabs } from "expo-router";
+import { Redirect, Tabs } from "expo-router";
 import { Image, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const tabBar = components.tabBar;
+
 const TabIcon = ({ focused, icon }: TabIconProps) => {
   return (
     <View className="tabs-icon">
@@ -16,18 +18,18 @@ const TabIcon = ({ focused, icon }: TabIconProps) => {
   );
 };
 const TabLayout = () => {
-  // //const { isSignedIn, isLoaded } = useAuth();
+  const { isSignedIn, isLoaded } = useAuth();
   const insets = useSafeAreaInsets();
 
-  // // Wait for auth to load before rendering anything
-  // if (!isLoaded) {
-  //     return null;
-  // }
+  // Wait for auth to load before rendering anything
+  if (!isLoaded) {
+    return null;
+  }
 
-  // // Redirect to sign-in if user is not authenticated
-  // if (!isSignedIn) {
-  //     return <Redirect href="/(auth)/sign-in" />;
-  // }
+  // Redirect to sign-in if user is not authenticated
+  if (!isSignedIn) {
+    return <Redirect href="/(auth)/sign-in" />;
+  }
 
   return (
     <Tabs
